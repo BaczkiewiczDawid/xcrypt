a9f043350c184ffdcb3cf803b50c771e3bb918c2
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2024-09-18 22:33:19 +0200

    feat: home page base design

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,11 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
+import type {Metadata} from "next";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+import {Montserrat} from "next/font/google"
+import {ThemeProvider} from "@/components/theme-provider";
+
+const montserrat = Montserrat({
+  subsets: ["latin"]
 });
 
 export const metadata: Metadata = {
@@ -19,17 +14,19 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
+                                     children,
+                                   }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="en" suppressHydrationWarning>
+    <body
+      className={`${montserrat.className} antialiased p-4 md:px-16 md:py-8`}
+    >
+    <ThemeProvider attribute={"class"} defaultTheme={"dark"} enableColorScheme enableSystem>
+      {children}
+    </ThemeProvider>
+    </body>
     </html>
   );
 }
